9558b07d1cb6bcee21a11b9a11442670516dca74
Jacob <jacobc1596@gmail.com>
2020-08-31 15:56:42 +0100

    Added fretboard theme option

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const strings = document.querySelectorAll('.string');
 const notes = document.querySelectorAll('.note');
 const loop = document.querySelector('.loop');
+const fretboard = document.querySelector('.fretboard')
 let audio = document.querySelector('audio');
+const themes = document.querySelectorAll('input[type="radio"]');
+console.log(themes);
 
+themes.forEach(theme => theme.addEventListener('change', updateBackground))
 notes.forEach(note => note.addEventListener('click', playNote));
-// strings.forEach(string => string.addEventListener('click', playNote))
-
-// loop.addEventListener('change', ()=>{
-//   if(playNoteRunning);
-// })
 
 function playNote(e){
   // const audio = document.querySelector(`audio[data-note="${e.target.dataset.note}"]`);
@@ -54,38 +53,13 @@ function playNote(e){
 
 }
 
-// function loopNote(e){
-//   // const audio = document.querySelector(`audio[data-note="${e.target.dataset.note}"]`);  
-//   console.log(e.target);
-//   audio.dataset.note = e.target.dataset.note;
-//   audio.attributes.src.nodeValue = `audio/${e.target.dataset.note}.wav `;
-//   console.log(audio);
-
-//   if(loop.checked){
-//     audio.loop = true;
-//   }
-
-//   audio.currentTime = 0;
-//   audio.play();
-
-//   //Add active classes
-//   let string = e.target.previousElementSibling;
-//   string.classList.add('vibrate');
-//   this.classList.add('playing');
-
-//   let note = e.target;
-//   string.classList.add('vibrate');
-//   note.classList.add('playing');
-
-//   //Remove active classes
-//   string.addEventListener('animationend', ()=>{
-//     string.classList.remove('vibrate');
-//     this.classList.remove('playing');
-//   })
-
-//   string.addEventListener('animationend', ()=>{
-//     string.classList.remove('vibrate');
-//     note.classList.remove('playing');
-//   });
-
-// }
+function updateBackground(e){
+  console.log(`${e.target.id} background applied`);
+  fretboard.classList.remove('dark');
+  fretboard.classList.remove('light');
+  fretboard.classList.remove('green');
+  fretboard.classList.remove('red');
+  fretboard.classList.remove('blue');
+  fretboard.classList.remove('brown');
+  fretboard.classList.add(e.target.id);
+}
