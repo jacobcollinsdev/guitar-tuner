36caacb2dbaa8e1e2f6c99c264bc548f710ba85f
Jacob <jacobc1596@gmail.com>
2020-09-01 13:36:45 +0100

    Added keydown events

--- script.js
+++ script.js
@@ -2,20 +2,94 @@ const strings = document.querySelectorAll('.string');
 const notes = document.querySelectorAll('.note');
 const loop = document.querySelector('.loop');
 const fretboard = document.querySelector('.fretboard')
-let audio = document.querySelector('audio');
 const themes = document.querySelectorAll('input[type="radio"]');
-console.log(themes);
+let audio = document.querySelector('audio');
 
 themes.forEach(theme => theme.addEventListener('change', updateBackground))
 notes.forEach(note => note.addEventListener('click', playNote));
 
+window.addEventListener('keydown', playSound);
+
 function playNote(e){
-  // const audio = document.querySelector(`audio[data-note="${e.target.dataset.note}"]`);
-  console.log(e.target);
   audio.dataset.note = e.target.dataset.note;
   audio.attributes.src.nodeValue = `audio/${e.target.dataset.note}.wav `;
-  console.log(audio);
 
+  checkLoop();
+
+  audio.currentTime = 0;
+  audio.play();
+
+  //Add active classes
+  let string = e.target.previousElementSibling;
+  string.classList.add('vibrate');
+  let note = e.target;
+  note.classList.add('playing');
+
+  //Remove active classes
+
+  string.addEventListener('animationend', ()=>{
+    string.classList.remove('vibrate');
+    note.classList.remove('playing');
+  });
+
+}
+
+function updateBackground(e){
+  fretboard.classList.remove('dark');
+  fretboard.classList.remove('light');
+  fretboard.classList.remove('green');
+  fretboard.classList.remove('red');
+  fretboard.classList.remove('blue');
+  fretboard.classList.remove('brown');
+  fretboard.classList.add(e.target.id);
+}
+
+function playSound(e){
+  console.log(e.key);
+  let lowE = "audio/Low-E.wav";
+  let aKey = "audio/A.wav";
+  let dKey = "audio/D.wav";
+  let gKey = "audio/G.wav";
+  let bKey = "audio/B.wav";
+  let highE = "audio/High-E.wav";
+
+  switch(e.key){
+    case "6":
+      audio.attributes.src.nodeValue = lowE;
+      break;
+    case "5":
+      audio.attributes.src.nodeValue = aKey;
+      break;
+    case "4":
+      audio.attributes.src.nodeValue = dKey;
+      break;
+    case "3":
+      audio.attributes.src.nodeValue = gKey;
+      break;
+    case "2":
+      audio.attributes.src.nodeValue = bKey;
+      break;
+    case "1":
+      audio.attributes.src.nodeValue = highE;
+      break;
+    default:
+      return;
+  }
+
+  checkLoop();
+
+  audio.play()
+  notes[notes.length - e.key].classList.add('playing');
+  strings[strings.length - e.key].classList.add('vibrate');
+
+  strings.forEach(string => string.addEventListener('animationend', ()=>{
+    notes[notes.length - e.key].classList.remove('playing');
+  strings[strings.length - e.key].classList.remove('vibrate');
+  }))
+}
+
+
+function checkLoop(){
   if(loop.checked){
     audio.loop = true;
   }else{
@@ -27,39 +101,4 @@ function playNote(e){
       audio.loop = false;
     }
   })
-
-  audio.currentTime = 0;
-  audio.play();
-
-  //Add active classes
-  let string = e.target.previousElementSibling;
-  string.classList.add('vibrate');
-  this.classList.add('playing');
-
-  let note = e.target;
-  string.classList.add('vibrate');
-  note.classList.add('playing');
-
-  //Remove active classes
-  string.addEventListener('animationend', ()=>{
-    string.classList.remove('vibrate');
-    this.classList.remove('playing');
-  })
-
-  string.addEventListener('animationend', ()=>{
-    string.classList.remove('vibrate');
-    note.classList.remove('playing');
-  });
-
-}
-
-function updateBackground(e){
-  console.log(`${e.target.id} background applied`);
-  fretboard.classList.remove('dark');
-  fretboard.classList.remove('light');
-  fretboard.classList.remove('green');
-  fretboard.classList.remove('red');
-  fretboard.classList.remove('blue');
-  fretboard.classList.remove('brown');
-  fretboard.classList.add(e.target.id);
 }
